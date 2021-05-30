fd44fb1bd8d8246ec50dc024277a140d856eff28
Wangjian <415895535@qq.com>
2021-05-30 20:57:15 +0800

    hot fix add

--- src/main/java/Gittest.java
+++ src/main/java/Gittest.java
@@ -2,5 +2,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("defo");
         System.out.println("test11");
+        System.out.println("hot0fix add");
     }
 }
